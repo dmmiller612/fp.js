d58f85531b86680637066921f384c6832a7a46aa
Derek Miller <derek.miller@inin.com>
2015-07-14 20:28:43 -0400

    Added more functions

--- fp.js
+++ fp.js
@@ -1,6 +1,44 @@
 var fp = (
 
     function(){
+        var memothunk = function(func){
+            var cache = func();
+            return function(){
+                return cache;
+            }
+        };
+
+        var thunk = function(value){
+            return function(){
+                return value;
+            }
+        };
+
+        var curry = function (func) {
+            return function innerFunc() {
+                var innerArgs = Array.prototype.slice.call(arguments, 0);
+                if (innerArgs.length >= func.length) {
+                    return func.apply(null, innerArgs);
+                }
+                else {
+                    return function () {
+                        return innerFunc.apply(null, innerArgs.concat(Array.prototype.splice.call(arguments, 0)));
+                    }
+                }
+            };
+        };
+
+        var variadic = function(args){
+            var func = args[0];
+            var rest = Array.prototype.slice.call(args, 1);
+            function f1(curryFunc, tailNum){
+                if (tailNum === rest.length) {
+                    return curryFunc;
+                }
+                return f1(curryFunc(rest[tailNum]), tailNum + 1);
+            }
+            return f1(curry(func), 0);
+        };
 
         var Maybe = function(value){
             var self = this;
@@ -31,6 +69,73 @@ var fp = (
             }
         };
 
+        var Stream = function(head, tail){
+            var self = this;
+            self.tail = tail;
+            self.head = memothunk(head);
+
+            var cons = function(head, tail){
+                return new Stream(head, tail);
+            };
+
+            self.curryFoldRight = function(defaultValue){
+                return function(func){
+                    if (self.head()){
+                        var tl = self.tail;
+                        return func(self.head(), function(){
+                            return tl().curryFoldRight(defaultValue)(func);
+                        });
+                    } else {
+                        return defaultValue();
+                    }
+                }
+            };
+
+            self.foldRight = function(func){
+                if (self.head()){
+                    var tl = self.tail;
+                    return func(self.head(), function(){
+                        return tl().foldRight(func);
+                    });
+                } else {
+                    return cons(thunk(null), thunk(null));
+                }
+            };
+
+            self.append = function(aTail){
+                return self.curryFoldRight(aTail)(function(h, t){
+                    return cons(thunk(h), t);
+                })
+            };
+
+            self.flatMap = function(func){
+                return self.foldRight(function(h, t){
+                    return func(h).append(t);
+                });
+            };
+
+            self.map = function(func){
+                return self.foldRight(function(h, t){
+                    return cons(thunk(func(h)), t);
+                });
+            };
+
+            self.toList = function(){
+                if (self.head()){
+                    return [self.head()].concat(self.tail().toList());
+                } else {
+                    return [];
+                }
+            };
+
+            self.filter = function(func){
+                return self.foldRight(function(h,t){
+                    if (func(h)) { return cons(thunk(h), t); }
+                    else return t();
+                });
+            }
+        };
+
         return {
             /**
              * Curry Example:
@@ -43,17 +148,7 @@ var fp = (
              * @returns {Function}
              */
             curry: function (func) {
-                return function innerFunc() {
-                    var innerArgs = Array.prototype.slice.call(arguments, 0);
-                    if (innerArgs.length >= func.length) {
-                        return func.apply(null, innerArgs);
-                    }
-                    else {
-                        return function () {
-                            return innerFunc.apply(null, innerArgs.concat(Array.prototype.splice.call(arguments, 0)));
-                        }
-                    }
-                };
+                return curry(func);
             },
 
             /**
@@ -119,18 +214,40 @@ var fp = (
              */
             maybe: function (value) {
                 return new Maybe(value);
+            },
+
+            /**
+             * MemoThunk example
+             *
+             * var z = memothunk(function(){
+             *     console.log("asdf");
+             *     return 5;
+             * });
+             * 
+             * @param func
+             * @returns {Function}
+             */
+            memothunk: function(func){
+                return memothunk(func);
+            },
+
+            variadic: function(/** function, restOfArgs */){
+                return variadic(arguments);
+            },
+
+            /**
+             * stream example (lazy)
+             * stream([1,2,3,4,5])
+             *              .map(function(val) {console.log("map"+val); return val + 10; })
+             *              .filter(function(val) { console.log("filter"+val); return val % 2 === 0})
+             *              .toList()
+             * @param values
+             * @returns {Stream}
+             */
+            stream: function(values){
+                if (values.length === 0) return new Stream(thunk(null), thunk(null));
+                return new Stream(thunk(values.shift()), thunk(stream(values)));
             }
         }
     }
 )();
-
-/**
- * Curry Function.prototype Example:
- * var blah = function(a,b,c){ return[a,b,c]; }.curry();
- * console.log(blah("a")("e")("r"));
- *
- * @returns {Function}
- */
-Function.prototype.curry = function(){
-    return fp.curry(this);
-};
